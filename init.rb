84d2cd572bfe2f5bc8e0e15318ad42f3e0efb0d4
Alexey Kinev <rudy@05bit.com>
2019-07-15 00:47:54 -0400

    Fix: set Redmine 4.0+ version support instead of strictly 4.0

--- init.rb
+++ init.rb
@@ -2,13 +2,15 @@ Redmine::Plugin.register :redmine_default_custom_query do
   name 'Redmine Default Custom Query'
   author 'Katsuya Hidaka'
   description 'Redmine plugin for setting default custom query of Issues for each project'
-  version '1.4.0'
-  requires_redmine '4.0'
+  version '1.4.1'
+  requires_redmine version_or_higher: '4.0'
   url 'https://github.com/hidakatsuya/redmine_default_custom_query'
   author_url 'https://twitter.com/hidakatsuya'
 
   project_module :default_custom_query do
-    permission :manage_default_query, { default_custom_query_setting: [ :update ] }, require: :member
+    permission :manage_default_query, {
+      default_custom_query_setting: [ :update ]
+    }, require: :member
   end
 end
 
